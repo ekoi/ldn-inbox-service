f0b3fe0495b7f3110a60af7d03f7fd238f598b86
ekoi <eko.indarto@dans.knaw.nl>
2023-02-13 12:16:03 +0100

    Intermediate commit: Add some comments

--- src/protected.py
+++ src/protected.py
@@ -23,24 +23,38 @@ async def get_settings():
 @router.head("/inbox")
 @router.post('/inbox')
 async def post_inbox(submitted_json_ld: Request):
+    # Get the JSON-LD object from the submitted request
     s_json_ld_obj = await submitted_json_ld.json()
+
+    # Create a graph to parse the JSON-LD data
     graph = Graph()
+
+    # Get the content type from the request headers
     content_type = submitted_json_ld.headers['Content-Type']
+
+    # Check if the content type is one of the accepted content types
     if content_type in ACCEPTED_CONTENT_TYPES:
-        print(content_type)
+
+        # Parse the JSON-LD data into the graph
         graph.parse(data=s_json_ld_obj, format=content_type)
+
+        # Serialize the graph as Turtle text
         text_turtle = graph.serialize(format='text/turtle')
-        # create a database connection
+
+        # Create a database connection
         conn = db.create_sqlite3_connection(settings.data_db_file)
         with conn:
-            # create a new record
+            # Create a new record for the inbox
             record = (
                 str(uuid.uuid4()), datetime.now().strftime("%m/%d/%Y %H:%M:%S.%f"), submitted_json_ld.client.host, json.dumps(s_json_ld_obj),
                 text_turtle);
+            # Insert the record into the inbox database
             record_id = db.create_inbox_record(conn, record)
-            print(record_id)
+            # Return a JSON response with a 201 Created status code and the record ID
             return JSONResponse(status_code=status.HTTP_201_CREATED, content=record_id, headers=headers(record_id))
     else:
+        # Return a 401 Unauthorized status code if the content type is not accepted
         return 401
 
+    # Return a 201 Created status code
     return 201
